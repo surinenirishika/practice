7a189ee1852270e3a23d6df91cd256dae7a11967
rishikasurineni <rishikas@vt.edu>
2021-10-26 20:05:00 -0400

    final changes

--- src/twoSum.py
+++ src/twoSum.py
@@ -2,19 +2,25 @@
 Created on Oct 26, 2021
 
 @author: rishikasurineni
+"""
+:type nums: List[int]
+:type target: int
+:rtype: List[int]
 '''
-def twoSum(self, nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    
-    
-    def helper(nums,target,i,j):
-        for i,j in nums[i],nums[i+1]:
-            if nums[i]+nums[j] == target:
-                return [i,j]
+def twoSum(nums: list[int], target: int) -> list[int]:
+       
+    nums1 = []
+    i = 0
+    j = 1
+    while i <= len(nums)-1 and j <= len(nums):
+        if nums[i]+nums[j] == target:
+            nums1.append(i)
+            nums1.append(j)
+            return nums1
         else:
-            helper(nums, target, i+1,j)
-            
+            if j <= len(nums):
+                j += 1
+            i += 1
+    return nums1
+print(twoSum([2,7,11,13],9))
+print(twoSum([3,2,4], 6))
--- src/twoSumTest.py
+++ src/twoSumTest.py
@@ -4,7 +4,7 @@ Created on Oct 26, 2021
 
 @author: rishikasurineni
 '''
-
+ 
 import unittest
 from twoSum import twoSum
 
@@ -16,3 +16,5 @@ class twoSumTest(unittest.TestCase):
     def test(self):
         #assert statements
         self.assertEqual(twoSum([2,7,11,15],9), [0,1])
+        self.assertEqual(twoSum([3,2,4], 6), [1,2])
+        
